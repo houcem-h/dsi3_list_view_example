203fe1e9f48b0c642b76c0a451461b9f22dc2421
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-29 10:15:56 +0100

    enhance display

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Luxury cars gallery'),
     );
   }
 }
@@ -34,17 +34,32 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final List<Car> _cars = [
     Car('Aston Martin', 'Virage', Image.asset('images/aston_martin_virage.jpg')),
+    Car('Porshe', '911', Image.asset('images/porshe_911.jpg')),
+    Car('Bugatti', 'Chiron', Image.asset('images/bugatti_chiron.jpg')),
     Car('Audi', 'R8', Image.asset('images/audi_r8.jpg')),
     Car('BMW', 'i8', Image.asset('images/bmw_i8.jpg')),
+    Car('Mercedes', 'GLA', Image.asset('images/mercedes_gla.jpg')),
+    Car('Rolls Royce', 'Ghost', Image.asset('images/rolls_royce_ghost.jpg')),
+    Car('Porshe', 'Panamera', Image.asset('images/porshe_panamera.jpg')),
     Car('BMW', 'M4', Image.asset('images/bmw_m4.jpg')),
+    Car('Lamborghini', 'Huracan', Image.asset('images/lamborghini_huracan.jpg')),
+    Car('Mustang', 'GT Premium', Image.asset('images/mustang_gt_premium.jpg')),
     Car('BMW', 'M5', Image.asset('images/bmw_m5.jpg')),
+    Car('Mercedes', 'SLS', Image.asset('images/mercedes_sls.jpg')),
     Car('Bugatti', 'Chiron', Image.asset('images/bugatti_chiron.jpg')),
     Car('Dodge', 'Challenger', Image.asset('images/dodge_challenger.jpg')),
     Car('Lamborghini', 'Huracan', Image.asset('images/lamborghini_huracan.jpg')),
+    Car('BMW', 'i8', Image.asset('images/bmw_i8.jpg')),
     Car('Mercedes', 'GLA', Image.asset('images/mercedes_gla.jpg')),
+    Car('Dodge', 'Challenger', Image.asset('images/dodge_challenger.jpg')),
     Car('Mercedes', 'SLS', Image.asset('images/mercedes_sls.jpg')),
+    Car('Aston Martin', 'Virage', Image.asset('images/aston_martin_virage.jpg')),
+    Car('BMW', 'M5', Image.asset('images/bmw_m5.jpg')),
     Car('Mustang', 'GT Premium', Image.asset('images/mustang_gt_premium.jpg')),
+    Car('BMW', 'M4', Image.asset('images/bmw_m4.jpg')),
+    Car('Lamborghini', 'Huracan', Image.asset('images/lamborghini_huracan.jpg')),
     Car('Porshe', '911', Image.asset('images/porshe_911.jpg')),
+    Car('Audi', 'R8', Image.asset('images/audi_r8.jpg')),
     Car('Porshe', 'Panamera', Image.asset('images/porshe_panamera.jpg')),
     Car('Rolls Royce', 'Ghost', Image.asset('images/rolls_royce_ghost.jpg')),
   ];
@@ -74,7 +89,19 @@ class _MyHomePageState extends State<MyHomePage> {
                 );
               },
               child: ListTile(
-                title: Text(item.model),
+                title: Text(
+                  item.model,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.cyan,
+                      fontSize: 20
+                  ),
+                ),
+                subtitle: Text(
+                  item.brand,
+                  textAlign: TextAlign.center,
+                ),
               ),
           );
         }),
