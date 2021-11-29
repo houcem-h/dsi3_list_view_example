f5f297b3ae8a8594c4917b1a66332642452506eb
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-29 09:50:21 +0100

    create cars list

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './car.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -29,6 +31,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  final List<Car> _cars = [
+    Car('Aston Martin', 'Virage', Image.asset('images/aston_martin_virage.jpg')),
+    Car('Audi', 'R8', Image.asset('images/audi_r8.jpg')),
+    Car('BMW', 'i8', Image.asset('images/bmw_i8.jpg')),
+    Car('BMW', 'M4', Image.asset('images/bmw_m4.jpg')),
+    Car('BMW', 'M5', Image.asset('images/bmw_m5.jpg')),
+    Car('Bugatti', 'Chiron', Image.asset('images/bugatti_chiron.jpg')),
+    Car('Dodge', 'Challenger', Image.asset('images/dodge_challenger.jpg')),
+    Car('Lamborghini', 'Huracan', Image.asset('images/lamborghini_huracan.jpg')),
+    Car('Mercedes', 'GLA', Image.asset('images/mercedes_gla.jpg')),
+    Car('Mercedes', 'SLS', Image.asset('images/mercedes_sls.jpg')),
+    Car('Mustang', 'GT Premium', Image.asset('images/mustang_gt_premium.jpg')),
+    Car('Porshe', '911', Image.asset('images/porshe_911.jpg')),
+    Car('Porshe', 'Panamera', Image.asset('images/porshe_panamera.jpg')),
+    Car('Rolls Royce', 'Ghost', Image.asset('images/rolls_royce_ghost.jpg')),
+  ];
 
   @override
   Widget build(BuildContext context) {
